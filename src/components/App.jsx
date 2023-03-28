287489f8b548ddc81261eb62963f6a5c8d13fc48
primarch93 <sugonegor@gmail.com>
2023-03-29 00:06:54 +0300

    fix-false

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,34 @@
+import { Routes, Route, NavLink } from "react-router-dom";
+import Home from '../pages/Home';
+import Movies from '../pages/Movies';
+import MovieDetails from '../pages/MovieDetails';
+import Cast from './Cast';
+import Reviews from './Reviews';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+   <div>
+    <header>
+        <ul>
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/movies">Movies</NavLink>
+          </li>
+        </ul>
+    </header>
+   </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/movies" element={<Movies />} />
+      <Route path="/movies/:id" element={<MovieDetails />} >
+      <Route path="cast" element={<Cast />} />
+      <Route path="reviews" element={<Reviews />} />
+      </Route>
+<Route path = '*' element={<h1>404</h1>} />
+    </Routes>
+    </>
   );
 };
